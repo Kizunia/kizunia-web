693f8a69f5b13ea0ea1be576cb87990b95d9f7a2
ishan kulkarni <ishankulkarni13@gmail.com>
2026-08-02 11:05:23 +0530

    migrate schema

--- next/prisma/migrations/20260725175650_/migration.sql
+++
@@ -1,6 +0,0 @@
--- AlterEnum
-ALTER TYPE "public"."HackathonVisibility" ADD VALUE 'PRIVATE';
-
--- AlterTable
-ALTER TABLE "public"."hackathon" ALTER COLUMN "registrationFeeType" DROP NOT NULL,
-ALTER COLUMN "registrationFeeType" DROP DEFAULT;
