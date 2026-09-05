e6c54f5302c30e53130d0e632edcbee091f60c03
ishan kulkarni <ishankulkarni13@gmail.com>
2026-09-06 01:10:13 +0530

    chore: default theme to light

--- next/src/app/layout.tsx
+++ next/src/app/layout.tsx
@@ -39,8 +39,8 @@ export default function RootLayout({
       >
         <ThemeProvider
           attribute="class"
-          defaultTheme="dark"
-          enableSystem
+          defaultTheme="light"
+          // enableSystem
           disableTransitionOnChange
         >
           {children}
