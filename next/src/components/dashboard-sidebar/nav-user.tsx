e983e15c8d52c977928845206bf037c4adc4690d
ishan kulkarni <ishankulkarni13@gmail.com>
2026-09-07 00:52:48 +0530

    (sidebar): add sign in btn when the user is not signed in

--- next/src/components/dashboard-sidebar/nav-user.tsx
+++ next/src/components/dashboard-sidebar/nav-user.tsx
@@ -1,12 +1,16 @@
 "use client";
 
+import React from "react";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 import {
   BellIcon,
+  LogIn,
   LogOutIcon,
   MoreVerticalIcon,
   UserCircleIcon,
 } from "lucide-react";
+import { toast } from "sonner";
 
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import {
@@ -24,131 +28,139 @@ import {
   SidebarMenuItem,
   useSidebar,
 } from "@/components/ui/sidebar";
-import { authClient } from "@/lib/auth-client";
-import { toast } from "sonner";
-import { useRouter } from "next/navigation";
-import AccountSwitcher from "../auth/account-switch";
-import React from "react";
 import { Skeleton } from "../ui/skeleton";
 
-export function NavUser() {
-  // const [isLoading, setLoading] = useState(false);
-  // const [loading, setLoading] = useState<boolean>(false);
+import { authClient } from "@/lib/auth-client";
+import AccountSwitcher from "../auth/account-switch";
+import { Button } from "../ui/button";
 
+export function NavUser() {
   const router = useRouter();
   const { isMobile } = useSidebar();
 
   const currentSessionData = authClient.useSession();
-  const currentSession = currentSessionData.data
-  const isUserNotLoggedIn = !currentSessionData.isPending && !currentSessionData.isRefetching && !currentSession;
-  // functions
+  const currentSession = currentSessionData.data;
+
+  const isUserNotLoggedIn =
+    !currentSessionData.isPending &&
+    !currentSessionData.isRefetching &&
+    !currentSession;
+
   async function handleSignOut() {
-    // setLoading(true);
-    toast.loading("Signing out...", { id: "signout", description: null });
+    toast.loading("Signing out...", {
+      id: "signout",
+      description: null,
+    });
+
     const { error } = await authClient.signOut();
+
     if (error) {
-      toast.error("Didn't signed out", {
+      toast.error("Sign out failed", {
         id: "signout",
         description: error.message || "Something went wrong",
       });
-    } else {
-      toast.success("Signed out successfully", {
-        id: "signout",
-        description: "Redirecting...",
-      });
-      // setLoading(false);
-      router.push("/");
+
+      return;
     }
+
+    toast.success("Signed out successfully", {
+      id: "signout",
+      description: "Redirecting...",
+    });
+
+    router.push("/");
   }
 
   return (
     <SidebarMenu>
       <SidebarMenuItem>
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-           { (!isUserNotLoggedIn) && <SidebarMenuButton
-              size="lg"
-              className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
+        {isUserNotLoggedIn ? (
+          <SidebarMenuButton asChild size="lg">
+            <Button asChild variant={"outline"}>
+            <Link href="/sign-in">
+            <LogIn />
+              <span>Sign in</span>
+            </Link></Button>
+          </SidebarMenuButton>
+        ) : (
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <SidebarMenuButton size="lg">
+                {currentSession ? (
+                  <>
+                    <Avatar className="h-8 w-8 rounded-lg">
+                      <AvatarImage
+                        src={currentSession.user.image ?? undefined}
+                        alt={currentSession.user.name}
+                      />
+                      <AvatarFallback className="rounded-lg">
+                        {currentSession.user.name?.charAt(0).toUpperCase()}
+                      </AvatarFallback>
+                    </Avatar>
+
+                    <div className="grid flex-1 text-left text-sm leading-tight">
+                      <span className="truncate font-medium">
+                        {currentSession.user.name}
+                      </span>
+                      <span className="truncate text-xs">
+                        {currentSession.user.email}
+                      </span>
+                    </div>
+
+                    <MoreVerticalIcon className="ml-auto size-4" />
+                  </>
+                ) : (
+                  <Skeleton className="h-9 w-full" />
+                )}
+              </SidebarMenuButton>
+            </DropdownMenuTrigger>
+
+            <DropdownMenuContent
+              className="w-[--radix-dropdown-menu-trigger-width] min-w-56 rounded-lg"
+              side={isMobile ? "bottom" : "right"}
+              align="end"
+              sideOffset={4}
             >
-              {currentSession  ? (
-                <>
-                  <Avatar className="h-8 w-8 rounded-lg  ">
-                    <AvatarImage
-                      src={
-                        currentSession?.user.image
-                          ? currentSession?.user.image
-                          : undefined
-                      }
-                      alt={currentSession.user.name}
-                    />
-                    <AvatarFallback className="rounded-lg">User {currentSessionData.error?.message}</AvatarFallback>
-                  </Avatar>
-                  <div className="grid flex-1 text-left text-sm leading-tight">
-                    <span className="truncate font-medium">
-                      {currentSession.user.name}
-                    </span>
-                    <span className="truncate text-xs text-muted-foreground ">
-                      {currentSession.user.email}
-                    </span>
-                  </div>
-                  <MoreVerticalIcon className="ml-auto size-4" />
-                </>
-              ) : (
-                <Skeleton className="w-full h-9" />
-              )}
-            </SidebarMenuButton>}
-          </DropdownMenuTrigger>
-          <DropdownMenuContent
-            className="w-[--radix-dropdown-menu-trigger-width] min-w-56 rounded-lg"
-            side={isMobile ? "bottom" : "right"}
-            align="end"
-            sideOffset={4}
-          >
-            <DropdownMenuLabel>
-              {(currentSession && !currentSessionData.isPending && !currentSessionData.isRefetching)? (
-                <AccountSwitcher
-                  currentSessionData={currentSession}
-                  // sessions={sessions}
-                  // isLoading={loading}
-                  // setLoading={setLoading}
-                  className="w-full"
-                />
-              ) : (
-                <Skeleton className="w-full h-9" />
-              )}
-            </DropdownMenuLabel>
+              <DropdownMenuLabel>
+                {currentSession &&
+                !currentSessionData.isPending &&
+                !currentSessionData.isRefetching ? (
+                  <AccountSwitcher
+                    currentSessionData={currentSession}
+                    className="w-full"
+                  />
+                ) : (
+                  <Skeleton className="h-9 w-full" />
+                )}
+              </DropdownMenuLabel>
 
-            {/* <DropdownMenuSeparator /> */}
+              <DropdownMenuGroup>
+                <DropdownMenuItem asChild>
+                  <Link href="/user/account">
+                    <UserCircleIcon />
+                    <span>Account</span>
+                  </Link>
+                </DropdownMenuItem>
 
-            <DropdownMenuGroup>
+                <DropdownMenuItem asChild>
+                  <Link href="/user/notifications">
+                    <BellIcon />
+                    <span>Notifications</span>
+                  </Link>
+                </DropdownMenuItem>
+              </DropdownMenuGroup>
+
+              <DropdownMenuSeparator />
+
               <DropdownMenuItem asChild>
-                <Link href={"/user/account"}>
-                  <UserCircleIcon />
-                  <span>Account</span>
-                </Link>
+                <button className="w-full" onClick={handleSignOut}>
+                  <LogOutIcon />
+                  <span>Sign out</span>
+                </button>
               </DropdownMenuItem>
-              <DropdownMenuItem asChild>
-                <Link href={"/user/notifications"}>
-                  <BellIcon />
-                  <span>Notifications</span>
-                </Link>
-              </DropdownMenuItem>
-            </DropdownMenuGroup>
-
-            <DropdownMenuSeparator />
-
-            <DropdownMenuItem asChild>
-              {/* <Link href={"/logout"}>
-                <LogOutIcon />
-                <span> Log out</span>
-              </Link> */}
-              <button className="w-full" onClick={handleSignOut}>
-                <LogOutIcon />
-                <span> Sign out</span>
-              </button>
-            </DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        )}
       </SidebarMenuItem>
     </SidebarMenu>
   );
